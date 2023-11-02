b97e1c6889e398471b299c71a81d57abbfaf746e
mft14 <metalfort14@gmail.com>
2023-11-02 13:19:30 +0100

    added chatgpt request but no test yet

--- the_zusammenfasser.ps1
+++ the_zusammenfasser.ps1
@@ -18,15 +18,16 @@ $title = $response.ParsedHtml.title
 
 # Liste von zugelassenen Top-Level-Domains und zugehörigen Regex-Patterns
 $allowedDomains = @{
-    "www.chip.de" = '(?s)<div\sclass="mt-sm">(.*?)</div>'
-    "www.formel1.de" = '(?s)<p>(.*?)</p>'
     "www.bild.de" = '(?s)<p>(.*?)</p>'
-    "www.sueddeutsche.de" = '(?s)<p\sclass="\scss-13wylk3"\sdata-manual="paragraph">(.*?)</p>'
-    "www.tagesschau.de" = '(?s)<p\sclass="textabsatz\sm-ten\sm-offset-one\sl-eight\sl-offset-two\scolumns\stwelve">(.*?)</p>'
+    "www.chip.de" = '(?s)<div\sclass="mt-sm">(.*?)</div>'
+    "www.focus.de" = '(?s)<p>(.*?)</p>'
+    "www.formel1.de" = '(?s)<p>(.*?)</p>'
     "www.heise.de" = '(?s)<p>(.*?)</p>'
-    "www.spiegel.de" = '(?s)<p>(.*?)</p>'
     "www.pcwelt.de" = '(?s)<p>(.*?)</p>' # am Anfang und Ende ungewollte Linkhinweise etc. aber sonst passt
     "www.silicon.de" = '(?s)<p>(.*?)</p>'
+    "www.spiegel.de" = '(?s)<p>(.*?)</p>'
+    "www.sueddeutsche.de" = '(?s)<p\sclass="\scss-13wylk3"\sdata-manual="paragraph">(.*?)</p>'
+    "www.tagesschau.de" = '(?s)<p\sclass="textabsatz\sm-ten\sm-offset-one\sl-eight\sl-offset-two\scolumns\stwelve">(.*?)</p>'
     # mehr Webseiten hier
 }
 
@@ -59,6 +60,29 @@ if ($allowedDomains.ContainsKey($tld)) {
     # $extractedText = "Hier den extrahierten Text einfügen"
     # $extractedText | Set-Clipboard
     # Write-Host "Der extrahierte Text wurde in die Zwischenablage kopiert."
+
+    $apiKey = "IHR_API_SCHLÜSSEL_HIER"
+
+    $headers = @{
+        "Content-Type" = "application/json"
+        "Authorization" = "Bearer $apiKey"
+    }
+
+    $body = @{
+        prompt = "Kannst du folgenden Text zusammenfassen: $extractedText"
+        # prompt = "Was ist die Bedeutung des Lebens?"
+        max_tokens = 50
+    } | ConvertTo-Json
+
+    $baseUrl = "https://api.openai.com/v1/chat/completions"
+    $response = Invoke-RestMethod -Uri $baseUrl -Headers $headers -Method Post -Body $body
+
+    # Verarbeiten Sie die Antwort von OpenAI entsprechend Ihren Anforderungen
+    $response.choices[0].text
+
+
+
+
 } else {
     Write-Host "Die Toplevel-Domain $tld ist nicht in der Liste der zugelassenen Domains enthalten."
     Exit
